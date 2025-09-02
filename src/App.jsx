cfd75b93615a4f372a302bb8137e3767dd9f62b6
nandasultan <nandasultan10@gmail.com>
2025-09-02 14:46:12 +0800

    UPDATE CV

--- src/App.jsx
+++ src/App.jsx
@@ -491,7 +491,7 @@ function App() {
                   </div>
                 </a>
                 <a
-                  href="/nanda-portofolio/assets/CV-ANANDA-SULTAN.pdf"
+                  href="/nanda-portofolio/assets/NANDA-CV.pdf"
                   className="flex items-center gap-4 p-3 rounded-lg hover:bg-zinc-300 transition-colors"
                   target="_blank"
                   rel="noopener noreferrer"
